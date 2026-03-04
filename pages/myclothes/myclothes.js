39325c4e8acf135754dddec285aff124efb50bae
literathicat <3969763844@qq.com>
2026-03-04 21:06:30 +0800

    增加衣柜搜索的交互

--- pages/myclothes/myclothes.js
+++ pages/myclothes/myclothes.js
@@ -5,7 +5,57 @@ Page({
    * 页面的初始数据
    */
   data: {
+    showCategoryOptions: false,
+    categoryOptions: [
+      { id: 1, name: 'T恤' },
+      { id: 2, name: '针织衫' },
+      { id: 3, name: '衬衫' },
+      { id: 4, name: '卫衣' },
+      { id: 5, name: '外套' },
+      { id: 6, name: '裤子' },
+      { id: 7, name: '裙子' },
+      { id: 8, name: '连衣裙' },
+      { id: 9, name: '牛仔裤' },
+      { id: 10, name: '短裤' }
+    ],
+    selectedCategory: ''
+  },
 
+  /**
+   * 切换品类选项显示
+   */
+  toggleCategoryOptions() {
+    this.setData({
+      showCategoryOptions: !this.data.showCategoryOptions
+    });
+  },
+
+  /**
+   * 选择品类
+   */
+  selectCategory(e) {
+    const index = e.currentTarget.dataset.index;
+    const category = this.data.categoryOptions[index];
+    this.setData({
+      selectedCategory: category.name,
+      showCategoryOptions: false
+    });
+    wx.showToast({
+      title: `已选择：${category.name}`,
+      icon: 'success',
+      duration: 1500
+    });
+    // 这里可以触发实际的筛选逻辑
+    // this.filterByCategory(category.id);
+  },
+
+  /**
+   * 关闭品类选项
+   */
+  closeCategoryOptions() {
+    this.setData({
+      showCategoryOptions: false
+    });
   },
 
   /**
